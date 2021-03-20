8227528a929f88aad19bba13f23eb9cb7812d247
Martin Kampas <martin.kampas@ubedi.net>
2021-03-20 20:37:28 +0100

    Doc: Expand overview

--- sod/sod.py
+++ sod/sod.py
@@ -160,7 +160,61 @@ def cli(debug):
 
     Sod is meant for single-user, single-history use - it provides no means of
     replicating history between repositories or maintaining alternate
-    histories (Special-purpose)."""
+    histories (Special-purpose).
+
+
+    INITIALIZATION
+
+    Sod repository can be initialized with the 'sod init' command executed
+    under an existing directory.  Sod will store its data under a subdirectory
+    named '.sod'.  Initially, a Sod repository has no history.  Any
+    pre-existing files found under the repository at initialization time are
+    treated equally as files appearing later after initialization.
+
+
+    RECORDING CHANGES
+
+    Changes since the last commit, as well as the initially untracked content
+    under a freshly initialized repository, can be listed with the 'sod
+    status' command.
+
+    Recording changes is a two phase process.  First the changes to be
+    recorded with the next commit are prepared (staged) with the 'sod add'
+    command.  Changes can be added step-by-step with multiple 'sod add'
+    invocations and any change previously staged can be unstaged with the 'sod
+    reset' command during this preparation phase.  The 'sod status' command
+    lists changes that are staged for next commit separately from those that
+    are not staged.
+
+    Once finished, the staged changes can be recorded with the 'sod commit'
+    command.  All commits in repository history can be listed with the 'sod
+    log' command.
+
+
+    UNDOING CHANGES
+
+    If a particular revision of a file is to be restored, the digest recorded
+    by Sod can be used to locate an exact copy of that file revision e.g. on
+    a backup.  Sod can assist that with the 'sod restore' command, accompanied
+    by the 'sod aux' group of commands for management of the so called
+    auxiliary data stores, the possible sources of older file revisions.
+
+    An auxiliary data store provides one or more snapshots of the original Sod
+    repository together with an information on which revision the snapshot was
+    taken at (or more correctly "taken after" - a snapshot taken while
+    uncommitted changes existed does not fully match the said revision).
+
+    In the output of the 'sod log' command, each revision with snapshots
+    available is annotated with the snapshots listed as
+    '<aux-name>[/<snapshot-id>]', omitting the optional part for stores
+    providing just single snapshot.
+
+    The simplest form of an auxiliary data store is a plain copy of the
+    original Sod repository. It may be a local copy or a remote one, in the
+    latter case accessed via SSH. Use 'sox aux add --help-types' to learn about
+    the possible auxiliary data store types.
+
+    """
 
     if not cli.initialized:
         init_logging(debug=debug)
