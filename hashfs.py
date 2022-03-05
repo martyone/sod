c4d3354214180f538dff5c57e4ecf16529487701
Martin Kampas <martin.kampas@ubedi.net>
2022-03-05 17:15:58 +0100

    Add copyright notices to files

--- hashfs.py
+++ hashfs.py
@@ -2,6 +2,8 @@
 '''
 hashfs.py - mirrors a directory tree replacing data with hashes
 
+This file is part of sod.
+
 Caveats:
 
  * Inode generation numbers are not passed through but set to zero.
@@ -20,6 +22,21 @@ Caveats:
 
  * If you delete or rename files in the underlying file system, the
    passthrough file system will get confused.
+
+Copyright (C) 2020,2021 Martin Kampas <martin.kampas@ubedi.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright ©  Nikolaus Rath <Nikolaus.org>
 
